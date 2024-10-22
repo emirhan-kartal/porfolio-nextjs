970632c577576b53681a624476879cb9bd9bc161
MhaWTHoR <e.kartal115@gmail.com>
2024-10-22 18:42:50 +0300

    ikas contact-form update

--- src/pages/ikas/index.tsx
+++ src/pages/ikas/index.tsx
@@ -1,5 +1,6 @@
 import ContactForm from "@/components/composites/contact/contact-form";
 import FAQ from "@/components/composites/contact/faq";
+import ContentWrapper from "@/components/ui/content-wrapper";
 import { getDatabase } from "@/lib/db";
 import { FAQuestionWithId } from "@/types";
 import { Typography } from "@mui/material";
@@ -8,13 +9,19 @@ import Head from "next/head";
 export default function Page({ faqs }: { faqs: FAQuestionWithId[] }) {
     return (
         <>
-        <Head>
-            <meta name="description" content="İkas Hakkında Bilgi Almak İçin Aşağıdaki Formu Doldurabilirsiniz" />
-            <title>İkas Bilgi - Emirhan Kartal</title>
-        </Head>
-        <Typography variant="h1">
-            Uzmandan İkas hakkında ÜCRETSİZ bilgi almak için aşağıdaki formu doldurabilirsiniz.
-        </Typography>
+            <Head>
+                <meta
+                    name="description"
+                    content="İkas Hakkında Bilgi Almak İçin Aşağıdaki Formu Doldurabilirsiniz"
+                />
+                <title>İkas Bilgi - Emirhan Kartal</title>
+            </Head>
+            <ContentWrapper content>
+                <Typography variant="h1" sx={{fontSize:{xs:"2.5rem",sm:"3rem",lg:"4rem"}}}>
+                    Uzmandan İkas hakkında ÜCRETSİZ bilgi almak için aşağıdaki
+                    formu doldurabilirsiniz.
+                </Typography>
+            </ContentWrapper>
             <ContactForm />
             <FAQ faqs={faqs} />
         </>
